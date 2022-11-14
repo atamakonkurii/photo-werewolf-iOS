e3860cab3a12c94f1f15b04e185b8715f4dc8ab6
atamakonkurii <morisudesu@gmail.com>
2022-11-15 06:21:34 +0900

    Initial Commit

---
+++ photo-werewolf-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  photo-werewolf-iOS
+//
+//  Created by 太田和希 on 2022/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ photo-werewolf-iOS/photo_werewolf_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_werewolf_iOSApp.swift
+//  photo-werewolf-iOS
+//
+//  Created by 太田和希 on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct photo_werewolf_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
