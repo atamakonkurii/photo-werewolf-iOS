1f881827da69b818400763a5c6968e2dc91b6491
atamakonkurii <morisudesu@gmail.com>
2023-03-22 08:05:54 +0900

    cloudFunctionをhttp経由で呼べるようになった

--- photo-werewolf-iOS/Firebase/CloudFunctions/functions/src/index.ts
+++ photo-werewolf-iOS/Firebase/CloudFunctions/functions/src/index.ts
@@ -3,7 +3,10 @@ import * as functions from "firebase-functions";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+// https://asia-northeast1-photo-werewolf.cloudfunctions.net/helloWorldTokyo
+/* eslint-disable max-len */
+export const helloWorldTokyo = functions.region("asia-northeast1").https.onRequest((request, response) => {
+  /* eslint-enable max-len */
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Tokyo!");
 });
