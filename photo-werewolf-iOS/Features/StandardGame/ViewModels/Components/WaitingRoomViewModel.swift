7458b2044670156fd109a1303f85c2b1c4052240
atamakonkurii <morisudesu@gmail.com>
2023-03-30 07:56:20 +0900

    コメント追加

--- photo-werewolf-iOS/Features/StandardGame/ViewModels/Components/WaitingRoomViewModel.swift
+++ photo-werewolf-iOS/Features/StandardGame/ViewModels/Components/WaitingRoomViewModel.swift
@@ -5,8 +5,10 @@ class WaitingRoomViewModel {
 	lazy var functions = Functions.functions(region:"asia-northeast1")
 
 	func changeStatusToPhotoSelect(roomId: String) async {
+		// 部屋のステータスを写真選択中に変更する
 		await FirestoreApiClient.shared.updateStatusGameRoom(roomId: roomId, status: .photoSelect)
 
+		// ロールを割り振る
 		functions.httpsCallable("standardGameAssignRoles").call(["gameRoomId": roomId]) { (_, error) in
 			if let error = error  {
 				print("error:\(error)")
