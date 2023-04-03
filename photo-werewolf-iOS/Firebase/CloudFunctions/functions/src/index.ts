f4a603b642f3cae838cd12a35874198967f97947
atamakonkurii <morisudesu@gmail.com>
2023-04-04 08:29:02 +0900

    werewolfのphotoUrlを交換する

--- photo-werewolf-iOS/Firebase/CloudFunctions/functions/src/index.ts
+++ photo-werewolf-iOS/Firebase/CloudFunctions/functions/src/index.ts
@@ -16,9 +16,10 @@ export const standardGameAssignRoles = functions.region("asia-northeast1").https
 
   type GameUser = {
     id: string;
-    roll: string | null;
+    role: string | null;
   }[];
-  const gameUsers: GameUser = gameUsersSnapshot.docs.map((doc) => ({id: doc.id, roll: null, ...doc.data()}));
+
+  const gameUsers: GameUser = gameUsersSnapshot.docs.map((doc) => ({id: doc.id, role: null, ...doc.data()}));
 
   if (gameUsers.length < 3) {
     throw new functions.https.HttpsError("failed-precondition", "At least 3 players are required.");
@@ -33,7 +34,7 @@ export const standardGameAssignRoles = functions.region("asia-northeast1").https
   // Assign roles
   const werewolfCount = 2;
   for (let i = 0; i < gameUsers.length; i++) {
-    gameUsers[i].roll = i < werewolfCount ? "werewolf" : "villager";
+    gameUsers[i].role = i < werewolfCount ? "werewolf" : "villager";
   }
 
   // Update Firestore
@@ -41,7 +42,7 @@ export const standardGameAssignRoles = functions.region("asia-northeast1").https
 
   gameUsers.forEach((user) => {
     const userRef = db.doc(`rooms/${gameRoomId}/gameUsers/${user.id}`);
-    batch.update(userRef, {roll: user.roll});
+    batch.update(userRef, {role: user.role});
   });
 
   await batch.commit();
@@ -50,3 +51,47 @@ export const standardGameAssignRoles = functions.region("asia-northeast1").https
 });
 /* eslint-enable max-len */
 
+/* eslint-disable max-len */
+export const standardGameExchangePhotoUrl = functions.region("asia-northeast1").https.onCall(async (data, _context) => {
+  const gameRoomId = data.gameRoomId;
+
+  if (!gameRoomId) {
+    throw new functions.https.HttpsError("invalid-argument", "gameRoomId is required.");
+  }
+
+  const gameUsersSnapshot = await db.collection(`rooms/${gameRoomId}/gameUsers`).get();
+
+  type GameUser = {
+    id: string;
+    role: string;
+    photoUrl: string;
+    exchangePhotoUrl: string | null;
+  }[];
+
+  const gameUsers: GameUser = gameUsersSnapshot.docs.map((doc) => ({id: doc.id, role:doc.data().role, photoUrl: doc.data().photoUrl, exchangePhotoUrl: null, ...doc.data()}));
+
+  // もしroleがwerewolfであれば、他のwerewolfのphotoUrlをexchangePhotoUrlに保存する
+  // roleがvillagerであれば自信のphotoUrlをexchangePhotoUrlに保存する
+gameUsers.forEach((user) => {
+    if (user.role === "werewolf") {
+      const werewolfPhotoUrls = gameUsers.filter((user) => user.role === "werewolf").map((user) => user.photoUrl);
+      // 自分のphotoUrlを除外する
+      user.exchangePhotoUrl = werewolfPhotoUrls.filter((photoUrl) => photoUrl !== user.photoUrl)[0];
+    } else {
+      user.exchangePhotoUrl = user.photoUrl;
+    }
+  });
+
+  // Update Firestore
+  const batch = db.batch();
+
+  gameUsers.forEach((user) => {
+    const userRef = db.doc(`rooms/${gameRoomId}/gameUsers/${user.id}`);
+    batch.update(userRef, {exchangePhotoUrl: user.exchangePhotoUrl});
+  });
+
+  await batch.commit();
+
+  return {message: "Roles assigned successfully"};
+});
+/* eslint-enable max-len */
